efac36c66d8f03505402002b5e9b23453175d9ba
pahpohman <ulogondronggedi@gmail.com>
2025-07-31 02:23:28 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function newGame() {
+  alert("New Game dimulai!");
+  document.getElementById("menu").style.display = "none";
+  document.getElementById("game").style.display = "block";
+}
+
+function loadGame() {
+  alert("Load Game belum tersedia.");
+}
+
+function openSettings() {
+  alert("Settings belum tersedia.");
+}
+
+function openOptions() {
+  alert("Options belum tersedia.");
+}
