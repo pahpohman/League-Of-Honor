f7c03801d97f2ee015326eae522146d143ba6fb4
pahpohman <ulogondronggedi@gmail.com>
2025-07-31 11:48:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -15,37 +15,3 @@ function openSettings() {
 function openOptions() {
   alert("Options belum tersedia.");
 }
-
-<script>
-window.addEventListener("orientationchange", () => {
-  location.reload(); // muat ulang agar layout menyesuaikan
-});
-</script>
-
-/* Overlay peringatan orientasi */
-#orientation-warning {
-  display: none;
-  position: fixed;
-  z-index: 9999;
-  background: black;
-  color: white;
-  font-size: 20px;
-  text-align: center;
-  width: 100vw;
-  height: 100vh;
-  padding: 20px;
-  box-sizing: border-box;
-  justify-content: center;
-  align-items: center;
-}
-
-/* Sembunyikan game saat portrait */
-@media only screen and (orientation: portrait) {
-  #orientation-warning {
-    display: flex;
-  }
-
-  #game-content {
-    display: none;
-  }
-}
